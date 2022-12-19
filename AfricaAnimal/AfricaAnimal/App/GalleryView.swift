c12fde7ee3999636a8e74371c08083bf3b357bf8
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-12-19 14:42:58 +0530

    VGrid added to galleryView

--- AfricaAnimal/AfricaAnimal/App/GalleryView.swift
+++ AfricaAnimal/AfricaAnimal/App/GalleryView.swift
@@ -8,9 +8,73 @@
 import SwiftUI
 
 struct GalleryView: View {
+    //MARK: - Properties
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    //Simle Grid Definition
+    // 3 column layout
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//    ]
+    
+    //Efficient Grid Definition
+//    let gridLayout: [GridItem] =  Array(repeating: GridItem(.flexible()), count: 3)
+    
+    //Dyanmic Gridlayout
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    
+    func gridSwitch() {
+        gridLayout =  Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
+    }
+    //MARK: Body
+    
     var body: some View {
         ScrollView(.vertical) {
-            Text("Gallery View")
+            //MARK: Image
+            Image(selectedAnimal)
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 8))
+           
+            //MARK: Slider
+            
+            Slider(value: $gridColumn, in: 2...4, step: 1)
+                .padding()
+                .onChange(of: gridColumn) { newValue in
+                    print("New grid is", $gridColumn)
+                    gridSwitch()
+                }
+            
+            //MARK: Grid
+            VStack(alignment: .center, spacing: 30) {
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                hapticImpact.impactOccurred()
+                            }
+                    } // Loop
+                } // LazyVGrid
+                .animation(.easeIn)
+                .onAppear(){
+                    gridSwitch()
+                }
+            } // VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
