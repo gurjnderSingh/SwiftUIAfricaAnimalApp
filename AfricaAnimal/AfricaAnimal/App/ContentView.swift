35e3e050a51a27e9849033888e2d3f74d3f79606
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-12-20 22:57:01 +0530

    AnimalGridItemView

--- AfricaAnimal/AfricaAnimal/App/ContentView.swift
+++ AfricaAnimal/AfricaAnimal/App/ContentView.swift
@@ -16,17 +16,22 @@ struct ContentView: View {
     //MARK: - Body
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    }
-                }
-                
-            } //: List
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
+                    } //: List
+                } else {
+                    Text("GridView Active")
+                } //: Condition
+            } // Group
             .navigationBarTitle("Africa",displayMode: .large)
             .toolbar {
                 HStack(spacing: 16) {
---
+++ AfricaAnimal/AfricaAnimal/View/AnimalGridItemView.swift
@@ -0,0 +1,30 @@
+//
+//  AnimalGridItemView.swift
+//  AfricaAnimal
+//
+//  Created by Gurjinder Singh on 20/12/22.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    //MARK: - Properties
+    let animal: Animal
+    
+    //MARK: - Body
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
